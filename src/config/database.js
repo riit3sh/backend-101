a04355ed4458ac2ee927e2781115ddbb32824b46
riit3sh <riteshx626@gmail.com>
2025-12-29 22:13:30 +0530

    feat:Setting up the base,file structure and database connection. With .env file and .gitignore for node modules

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+
+const connectDB = async () => {
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`\n MongoDB connected: ${connectionInstance.connection.host}`);
+    }catch(error){
+        console.error("\n MongoDB connection error:",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
