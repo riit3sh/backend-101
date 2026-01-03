048680c482288ba952f601678bd18bb82a858952
riit3sh <riteshx626@gmail.com>
2026-01-03 23:42:13 +0530

    feat: Added CRUD apis

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ const app = express();
 app.use(express.json());
 
 import userRouter from '../routes/user.route.js';
-//import postRouter from '../routes/post.route.js';
+import postRouter from '../routes/post.route.js';
+
 
 
 app.use("/api/v1/users",userRouter);
-//app.use("/api/v1/posts",postRouter);
+//example route:https://localhost:4000/api/v1/users/register
+
+app.use("/api/v1/posts",postRouter);
 
 export default app;
